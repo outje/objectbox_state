46dd8e3c87f4f1395da88e9bc1a576d33c2d0b53
outje <lindabengtsson_@hotmail.com>
2021-12-09 15:52:48 +0100

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_objectbox_state/database/objectbox.dart';
 import 'package:flutter_objectbox_state/screens/my_home_page.dart';
+import 'package:flutter_objectbox_state/statemanagement/data_controller.dart';
+import 'package:provider/provider.dart';
 
 /// Provides access to the ObjectBox Store throughout the app.
 late ObjectBox objectBox;
@@ -17,12 +19,15 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => DataController(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
